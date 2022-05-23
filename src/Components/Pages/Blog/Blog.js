10704fd3013639f29aad74154f808f00ca22d44c
ullash4 <programmerullash@gmail.com>
2022-05-23 23:19:43 +0600

    purchase page setup

--- src/Components/Pages/Blog/Blog.js
+++ src/Components/Pages/Blog/Blog.js
@@ -2,7 +2,210 @@ import React from 'react'
 
 function Blog() {
   return (
-    <div>Blog</div>
+    <div className='max-w-4xl mx-auto my-10 px-10'>
+      <h1 className='text-3xl font-bold text-center mb-5'>Some Questions Answer</h1>
+      <div className="space-y-4">
+
+        {/* question 1 */}
+  <details className="group" open>
+    <summary
+      className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50"
+    >
+      <h5 className="font-medium text-gray-900">
+      How will you improve the performance of a React Application?
+      </h5>
+
+      <svg
+        className="flex-shrink-0 ml-1.5 w-5 h-5 transition duration-300 group-open:-rotate-180"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </summary>
+
+    <p className="px-4 mt-4 leading-relaxed text-gray-700">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+      molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+      voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+      earum similique!
+    </p>
+  </details>
+
+
+{/* question 2 */}
+  <details className="group">
+    <summary
+      className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50"
+    >
+      <h5 className="font-medium text-gray-900">
+      What are the different ways to manage a state in a React application?
+      </h5>
+
+      <svg
+        className="flex-shrink-0 ml-1.5 w-5 h-5 transition duration-300 group-open:-rotate-180"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </summary>
+
+    <p className="px-4 mt-4 leading-relaxed text-gray-700">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+      molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+      voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+      earum similique!
+    </p>
+  </details>
+
+  {/* question 3 */}
+  <details className="group">
+    <summary
+      className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50"
+    >
+      <h5 className="font-medium text-gray-900">
+      How does prototypical inheritance work?
+      </h5>
+
+      <svg
+        className="flex-shrink-0 ml-1.5 w-5 h-5 transition duration-300 group-open:-rotate-180"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </summary>
+
+    <p className="px-4 mt-4 leading-relaxed text-gray-700">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+      molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+      voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+      earum similique!
+    </p>
+  </details>
+
+  {/* question 4 */}
+  <details className="group">
+    <summary
+      className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50"
+    >
+      <h5 className="font-medium text-gray-900">
+      Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts
+      </h5>
+
+      <svg
+        className="flex-shrink-0 ml-1.5 w-5 h-5 transition duration-300 group-open:-rotate-180"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </summary>
+
+    <p className="px-4 mt-4 leading-relaxed text-gray-700">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+      molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+      voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+      earum similique!
+    </p>
+  </details>
+
+  {/* question 5 */}
+  <details className="group">
+    <summary
+      className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50"
+    >
+      <h5 className="font-medium text-gray-900">
+      You have an array of products. Each product has a name, price, description, etc. How will you implement a search to find products by name?
+      </h5>
+
+      <svg
+        className="flex-shrink-0 ml-1.5 w-5 h-5 transition duration-300 group-open:-rotate-180"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </summary>
+
+    <p className="px-4 mt-4 leading-relaxed text-gray-700">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+      molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+      voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+      earum similique!
+    </p>
+  </details>
+
+  {/* Question 6 */}
+  <details className="group">
+    <summary
+      className="flex items-center justify-between p-4 rounded-lg cursor-pointer bg-gray-50"
+    >
+      <h5 className="font-medium text-gray-900">
+      What is a unit test? Why should write unit tests?
+      </h5>
+
+      <svg
+        className="flex-shrink-0 ml-1.5 w-5 h-5 transition duration-300 group-open:-rotate-180"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M19 9l-7 7-7-7"
+        />
+      </svg>
+    </summary>
+
+    <p className="px-4 mt-4 leading-relaxed text-gray-700">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
+      molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
+      voluptate dicta quo officiis explicabo consequuntur distinctio corporis
+      earum similique!
+    </p>
+  </details>
+</div>
+    </div>
   )
 }
 
--- src/Components/Pages/MyPortfolio/MyPortfolio.js
+++ src/Components/Pages/MyPortfolio/MyPortfolio.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function MyPortfolio() {
   return (
-    <div>MyPortfolio</div>
-  )
+    <div className="my-10 px-14 lg:px-20">
+      <div class="card lg:card-side bg-base-100 shadow-xl">
+        <figure>
+          <img
+          className="w-[494px] h-[455px] shadow-lg"
+            src="https://i.ibb.co/W3Pbwz6/IMG-20220503-182007.jpg"
+            alt="Album"
+          />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title text-3xl font-bold">Imran Mahmud Ullash</h2>
+          <p className="font-bold text-xl">Email: programmerullash@gmail.com</p>
+          <p className="font-bold text-xl">Education: Diploma in computer 5th semester- Tangail Polytechnic Institute</p>
+          <p className="font-bold text-xl">Skills: Html, CSS, Bootstrap, TailwindCSS, Javascript, ES6, React.Js, React Router, React Query, AOS, Firebase, Node.Js, Express.Js, MongoDB</p>
+          <p className="font-bold text-xl flex flex-col lg:flex-row justify-center items-center gap-2 ">Some Projects click to see: 
+          <a className="btn btn-md btn-primary mx-2" href="https://sports-wizard-e919a.web.app/"  target="_blank">SportsWizard</a>
+          <a className="btn btn-md btn-primary mx-2" href="https://to-do-app-8fe72.web.app/"  target="_blank">To-do App</a>
+          <a className="btn btn-md btn-primary mx-2" href="https://photopidea.web.app/"  target="_blank">PhotoPidea</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default MyPortfolio
+export default MyPortfolio;
