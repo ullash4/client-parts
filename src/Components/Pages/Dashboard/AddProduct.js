ca8ec67a1e6eda0df804f61f2e701c162994770c
ullash4 <programmerullash@gmail.com>
2022-05-24 12:53:10 +0600

    add product page implement

--- src/Components/Pages/Dashboard/AddProduct.js
+++ src/Components/Pages/Dashboard/AddProduct.js
@@ -1,8 +1,226 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 
 function AddProduct() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const onSubmit=(data)=>{
+    const price=data.price;
+    const picture=data.picture;
+    const avquantity=data.avquantity;
+    const minquantity=data.minquantity;
+    const name=data.name;
+    const email=data.email;
+    const description=data.description;
+    const product={
+      price, picture, avquantity,minquantity,name,email,description 
+    }
+    fetch("http://localhost:5000/parts",{
+      method:"POST",
+      headers:{
+        'content-type':'application/json'
+      },
+      body: JSON.stringify(product)
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      console.log(data)
+      toast.success("successfully added a product")
+    })
+    console.log(data);
+  }
   return (
-    <div>AddProduct</div>
+    <div className='my-2 mb-10'>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Product Name</span>
+          </label>
+          <input
+            {...register("name", {
+              required: {
+                value: true,
+                message: "Name is required",
+              },
+            })}
+            type="text"
+            placeholder="Product Name"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <label className="label">
+            {errors.name?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.name.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            {...register("email", {
+              required: {
+                value: true,
+                message: "Email is required",
+              },
+              pattern: {
+                value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                message: "Please Give a valid Email",
+              },
+            })}
+            type="email"
+            placeholder="Your Email"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <label className="label">
+            {errors.email?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.email.message}
+              </span>
+            )}
+            {errors.email?.type === "pattern" && (
+              <span className="label-text-alt text-red-500">
+                {errors.email.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Description</span>
+          </label>
+          <textarea
+            {...register("description", {
+              required: {
+                value: true,
+                message: "Description is required",
+              },
+            })}
+            
+            placeholder="Description"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <label className="label">
+            {errors.description?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.description.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Minimum Quantity</span>
+          </label>
+          <input
+            {...register("minquantity", {
+              required: {
+                value: true,
+                message: "Minimum Quantity is required",
+              },
+            })}
+            type="number"
+            placeholder="Minimum Quantity"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <label className="label">
+            {errors.minquantity?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.minquantity.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Available Quantity</span>
+          </label>
+          <input
+            {...register("avquantity", {
+              required: {
+                value: true,
+                message: "Available Quantity is required",
+              },
+            })}
+            type="number"
+            placeholder="Available Quantity"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <label className="label">
+            {errors.avquantity?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.avquantity.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Price</span>
+          </label>
+          <input
+            {...register("price", {
+              required: {
+                value: true,
+                message: "Price Quantity is required",
+              },
+            })}
+            type="number"
+            placeholder="Price Quantity"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <label className="label">
+            {errors.price?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.price.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Image</span>
+          </label>
+          <input
+            {...register("image", {
+              required: {
+                value: true,
+                message: "Image is required",
+              },
+            })}
+            type="text"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <label className="label">
+            {errors.image?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.image.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <input
+          className="btn bg-black outline-none text-white hover:text-white btn-wide mt-5"
+          value="Add Product"
+          type="submit"
+        />
+      </form>
+    </div>
   )
 }
 
--- src/Components/Pages/Dashboard/MyOrder.js
+++ src/Components/Pages/Dashboard/MyOrder.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 function MyOrder() {
+  const [orders, setOrders] = useState([]);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/order?email=${user.email}`, {
+        method: "GET",
+      })
+        .then((res) => res.json())
+        .then((data) => setOrders(data));
+    }
+  }, [user]);
   return (
-    <div>MyOrder</div>
-  )
+    <div>
+      <h1 className="text-xl text-center">My orderment {orders.length}</h1>
+
+      <div className="overflow-x-auto ">
+        <table className="table w-full ">
+          <thead>
+            <tr>
+              <th>Number</th>
+              <th>Name</th>
+              <th>Order Name</th>
+              <th>Quantity</th>
+              <th>Payment</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order, index) => (
+              <tr className="hover" key={order._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={
+                            user?.photoURL ||
+                            "https://images.pexels.com/photos/5875794/pexels-photo-5875794.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                          }
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">
+                        {order.userName || "Normal guy"}
+                      </div>
+                      <div className="text-sm opacity-50">{order.email}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>{order.productname}</td>
+                <td>{order.quantity} pices</td>
+
+                <td>
+                  {order.productPrice && (
+                    <Link to={`payment/${order._id}`}>
+                      <button className="btn btn-xs">Pay</button>
+                    </Link>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default MyOrder
+export default MyOrder;
